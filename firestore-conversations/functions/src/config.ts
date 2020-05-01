442a6ae177edfc57f008873a57c2aa55815b26e6
marcel corso <marcel@messagebird.com>
2020-05-01 10:02:26 +0200

    hello conversations

---
+++ firestore-conversations/functions/src/config.ts
@@ -0,0 +1,6 @@
+export default {
+  location: process.env.LOCATION,
+  accessKey: process.env.ACCESS_KEY,
+  outboxCollection: process.env.OUTBOX_COLLECTION,
+  messagesCollection: process.env.MESSAGES_COLLECTION,
+};
